bbe1639c59238fe066f3ce55e5cee0f06b5c749a
jm-rives <joanna.rives@gmail.com>
2016-10-31 12:55:28 -0700

    ADD auto load file to application.rb

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -3,10 +3,17 @@ require 'httparty'
 class Slack_Api_Wrapper
   # will need the token
   TOKEN = ENV['SLACK_TOKEN']
-  BASE_URL = "https://slack.com/api
-  
-  def self.list_channels
+  BASE_URL = "https://slack.com/api"
 
+  # use postman documentation or to find adressess
+  def self.list_channels
+    # makes API call
+    url = BASE_URL + "channels.list?token=#{TOKEN}"
   end
+
+  # stores API response
+  response = HTTParty.get(url)
+
+  # will NOT work from rails console unless directed to seek the script in the lib folder
 
 end
