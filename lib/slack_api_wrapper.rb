130c5c55abe1cf3a944b95245a16be46e62c7627
jm-rives <joanna.rives@gmail.com>
2016-10-31 13:31:00 -0700

    ADD channels method

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,19 +1,50 @@
 require 'httparty'
+require 'channel'
 
 class Slack_Api_Wrapper
   # will need the token
   TOKEN = ENV['SLACK_TOKEN']
-  BASE_URL = "https://slack.com/api"
+  BASE_URL = "https://slack.com/api/"
 
   # use postman documentation or to find adressess
   def self.list_channels
     # makes API call
     url = BASE_URL + "channels.list?token=#{TOKEN}"
-  end
+  
 
   # stores API response
   response = HTTParty.get(url)
 
-  # will NOT work from rails console unless directed to seek the script in the lib folder
+  # ap response - for print debugging'
+
+  #will NOT work from rails console unless directed to seek the script in the lib folder
+
+  my_channels = []
+
+  response["channels"].each do |channel|
+    id = channel["id"]
+    name = channel["name"]
+    my_channels << Slack_Channel.new(name, id)
+    end
+
+    my_channels
+  end
+
+  def self.send_message(channel, msg)
+
+  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
